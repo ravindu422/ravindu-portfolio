7450b24919de0067e6c5aa64498688018636d646
ravindu422 <thiranjayaravindu@gmail.com>
2025-07-04 13:30:53 +0530

    create hero section

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='home' className='relative min-h-screen flex items-center justify-center px-4'>
+        <div className='text-center'>
+            <div>
+                <h1></h1>
+                <p></p>
+                <p></p>
+                <div>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
